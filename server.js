e065b3862f6a97b76cd4fae40c1f40b52fa2f58c
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-12-05 15:21:59 -0600

    blog model

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const session = require('express-session')
 const { engine } = require('express-handlebars')
 const routes = require('./controllers')
 const sequelize = require('./config/connection')
-// const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
-// const sess = {
-//     //does this work?
-//     secret: 'secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// }
+const sess = {
+    //does this work?
+    secret: 'secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
 
-// app.use(session(sess))
+app.use(session(sess))
 
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')

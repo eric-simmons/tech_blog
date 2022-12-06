01c98798440b1257bb76ff409c0e228f3b96fb67
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-12-06 11:18:24 -0600

    seeds working

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,9 @@
 const sequelize = require('../config/connection')
-const { User } = require('../models')
+const { User, Comment, Blog } = require('../models')
 
 const userData = require('./userData.json')
+const blogData = require('./blogData.json')
+const commentData = require('./commentData.json')
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true })
@@ -11,6 +13,19 @@ const seedDatabase = async () => {
         returning: true,
     })
 
+    // for (const blog of blogData){
+    //     await Blog.create({...blog, user_id: users[Math.floor(Math.random()* users.length)].id})
+    // }
+    const blogs = await Blog.bulkCreate(blogData, {
+        individualHooks: true, 
+        returning: true,
+    })
+
+    const comments = await Comment.bulkCreate(commentData, {
+        individualHooks: true, 
+        returning: true,
+    })
+console.log(users, blogs, comments)
     process.exit(0)
 }
 

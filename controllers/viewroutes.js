1d2698c23f9f1ea17896b27dc0a8a98e0a66c2c2
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-12-06 13:25:40 -0600

    login/logout helpers and views

--- controllers/viewroutes.js
+++ controllers/viewroutes.js
@@ -1,8 +1,30 @@
+const { Blog } = require('../models')
+
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.render('home')
+router.get('/', async (req, res) => {
+    try {
+        let blogs = await Blog.findAll()
+        blogs = blogs.map(blog => blog.get({ plain: true }))
+        console.log(blogs)
+        res.render('home', { blogs })
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
 })
 
+
+
+router.get('/blog/:id', async (req, res) => {
+    try {
+        let blog = await Blog.findByPk(req.params.id)
+        blog = blog.get({ plain: true })
+        res.render('blog', { blog })
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router

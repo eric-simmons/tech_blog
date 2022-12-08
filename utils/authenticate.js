eef67631748192586b8a9f802ab53f9f664d0cfe
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-12-08 17:59:59 -0600

    recent

--- utils/authenticate.js
+++ utils/authenticate.js
@@ -1,15 +1,5 @@
 const authenticate = (req, res, next) => {
-   
    !req.session.logged_in ? res.redirect('/login') : next()
-   
-   
-   
-   
-    // if (!req.session.logged_in) {
-    //     res.redirect('/login')
-    // } else{
-    //     next()
-    // }
 }
 
 module.exports = authenticate
